08d8618babdec4031802ee96087c8c371272d08b
Kumar <akk919899@tatatechnologies.com>
2024-10-15 16:34:35 +0530

    base url

--- webAccess/config/constants.js
+++ webAccess/config/constants.js
@@ -1,6 +1,6 @@
 let Config = require('../../bin/config');
 module.exports = {
 	//"baseUrl": `http://3.110.218.134:${Config.ServerList.MainApplicationPort}/`,
-	"baseUrl": `http://3.110.218.134/`,
+	"baseUrl": `http://13.232.118.115/`,
 	"siteName": "School Management",		
 }
